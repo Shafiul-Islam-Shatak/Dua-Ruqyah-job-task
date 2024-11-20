20ff7c1e12fd23193d8c677c569987df73b33cfd
Shatak-the-imposter <helloshatak@gmail.com>
2024-11-20 20:13:55 +0600

    first commit

---
+++ src/app/page.js
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+
+export default function Home() {
+  return (
+    <div className="">
+      
+      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+        hello
+      </main>
+      
+    </div>
+  );
+}
